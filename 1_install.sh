39a347cd54a3ad9e41054deec6e9b28120479873
Gastón Martínez <gastongmartinez@gmail.com>
2025-05-28 15:19:26 -0300

    Actualiza repo M$

--- 1_install.sh
+++ 1_install.sh
@@ -47,7 +47,7 @@ fi
 # Repositorios VSCode y Powershell 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo
+curl https://packages.microsoft.com/config/rhel/9/prod.repo | tee /etc/yum.repos.d/microsoft.repo
 dnf check-update
 dnf makecache
 
