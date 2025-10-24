e2cdf77e6418d606826c95d0855e0a2fc41a4739
Gastón Martínez <gastongmartinez@gmail.com>
2025-10-24 10:21:38 -0300

    Actualiza RPMs

--- 1_install.sh
+++ 1_install.sh
@@ -295,9 +295,9 @@ if [[ $GAPPS =~ ^[Ss]$ ]]; then
 fi
 
 dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
-dnf install https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.rpm -y
 dnf install https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.rpm -y
-dnf install https://download2.gluonhq.com/scenebuilder/24.0.1/install/linux/SceneBuilder-24.0.1.rpm -y
+dnf install https://download.oracle.com/java/25/latest/jdk-25_linux-x64_bin.rpm -y
+dnf install https://download2.gluonhq.com/scenebuilder/25.0.0/install/linux/SceneBuilder-25.0.0.rpm -y
 dnf install https://download.oracle.com/otn_software/java/sqldeveloper/sqldeveloper-24.3.1-347.1826.noarch.rpm -y
 dnf install https://download.oracle.com/otn_software/java/sqldeveloper/datamodeler-24.3.1.351.0831-1.noarch.rpm -y
 ###############################################################################
