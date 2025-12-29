d20dfda19d4d05dd2375741fd46c03f0ce569950
Gastón Martínez <gastongmartinez@gmail.com>
2025-12-29 11:52:06 -0300

    Agrega NiriWM

---
+++ Niri/3_niri.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Validacion del usuario ejecutando el script
+R_USER=$(id -u)
+if [ "$R_USER" -ne 0 ]; then
+    echo -e "\nDebe ejecutar este script como root o utilizando sudo.\n"
+    exit 1
+fi
+
+# Extra Repos
+dnf copr enable avengemedia/dms -y
+dnf copr enable brycensranch/gpu-screen-recorder-git -y
+
+# Instalación Niri & DankMaterialShell
+dnf install niri dms -y
+
+# Dependencias Noctalia Shell
+dnf install ddcutil -y
+dnf install brightnessctl -y
+dnf install gpu-screen-recorder-ui -y
+dnf install wlsunset -y
+dnf install evolution-data-server -y
---
+++ Niri/4_niri_user.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+R_USER=$(id -u)
+if [ "$R_USER" -eq 0 ]; then
+   echo "Este script debe usarse con un usuario regular."
+   echo "Saliendo..."
+   exit 1
+fi
+
+# Instalacion noctalia-shell
+mkdir -p ~/.config/quickshell/noctalia-shell
+curl -sL https://github.com/noctalia-dev/noctalia-shell/releases/latest/download/noctalia-latest.tar.gz | tar -xz --strip-components=1 -C ~/.config/quickshell/noctalia-shell
+
+# Servicios de usuario
+systemctl --user enable --now dsearch
+systemctl --user add-wants niri.service dms
+
