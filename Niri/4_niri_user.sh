872aea20aa37a9b99f94955f60c263980ba314e3
Gastón Martínez <gastongmartinez@gmail.com>
2025-12-30 12:57:29 -0300

    Agrega temas para Niri

--- Niri/4_niri_user.sh
+++ Niri/4_niri_user.sh
@@ -15,3 +15,46 @@ curl -sL https://github.com/noctalia-dev/noctalia-shell/releases/latest/download
 systemctl --user enable --now dsearch
 systemctl --user add-wants niri.service dms
 
+# Cursores WhiteSur
+read -rp "Instalar WhiteSur Cursors? (S/N): " CUR
+if [[ $CUR =~ ^[Ss]$ ]]; then
+   git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+   cd WhiteSur-cursors || return
+   ./install.sh
+   cd ..
+   rm -rf WhiteSur-cursors
+fi
+
+# Tema WhiteSur GTK
+read -rp "Instalar WhiteSur GTK? (S/N): " GTK
+if [[ $GTK =~ ^[Ss]$ ]]; then
+   pkill firefox
+   git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
+   cd WhiteSur-gtk-theme || return
+   ./install.sh -l -N glassy --shell -i fedora
+   ./tweaks.sh -f
+   ./tweaks.sh -F
+   sudo flatpak override --filesystem=xdg-config/gtk-4.0
+   if [ ! -d ~/.themes ]; then
+       mkdir -p ~/.themes
+       tar -xf ./release/WhiteSur-Dark.tar.xz -C ~/.themes/
+   fi
+   cd ..
+   rm -rf WhiteSur-gtk-theme
+fi
+
+# Tema WhiteSur KDE
+read -rp "Instalar WhiteSur KDE? (S/N): " KDE
+if [[ $KDE =~ ^[Ss]$ ]]; then
+   git clone https://github.com/vinceliuice/WhiteSur-kde.git
+   cd WhiteSur-kde || return
+   ./install.sh
+   read -rp "Instalar Tema WhiteSur SDDM? (S/N): " SDDM
+   if [[ $SDDM =~ ^[Ss]$ ]]; then
+      cd sddm || return
+      sudo ./install.sh
+   fi
+   cd ..
+   rm -rf WhiteSur-kde
+fi
+
