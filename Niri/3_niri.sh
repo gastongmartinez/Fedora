60d996f74513129359a07fda22072c1d693a5539
Gastón Martínez <gastongmartinez@gmail.com>
2026-02-11 11:57:14 -0300

    Deshabilita instalacion dep Noctalia

--- Niri/3_niri.sh
+++ Niri/3_niri.sh
@@ -10,13 +10,14 @@ fi
 # Extra Repos
 dnf copr enable avengemedia/dms -y
 dnf copr enable brycensranch/gpu-screen-recorder-git -y
+dnf copr enable avengemedia/danklinux -y
 
 # Instalación Niri & DankMaterialShell
 dnf install niri dms -y
 
 # Dependencias Noctalia Shell
-dnf install ddcutil -y
-dnf install brightnessctl -y
-dnf install gpu-screen-recorder-ui -y
-dnf install wlsunset -y
-dnf install evolution-data-server -y
+#dnf install ddcutil -y
+#dnf install brightnessctl -y
+#dnf install gpu-screen-recorder-ui -y
+#dnf install wlsunset -y
+#dnf install evolution-data-server -y
