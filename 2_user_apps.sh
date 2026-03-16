cfb5b622ce3fb82b2f6dfe260ec9fab971b1e628
Gastón Martínez <gastongmartinez@gmail.com>
2026-03-16 15:55:21 -0300

    Actualiza Marksman

--- 2_user_apps.sh
+++ 2_user_apps.sh
@@ -92,7 +92,7 @@ wget https://github.com/JohnnyMorganz/StyLua/releases/download/v2.4.0/stylua-lin
 unzip stylua-linux-x86_64.zip -d "$HOME/.local/bin/"
 rm stylua-linux-x86_64.zip
 
-wget https://github.com/artempyanykh/marksman/releases/download/2024-12-18/marksman-linux-x64
+wget https://github.com/artempyanykh/marksman/releases/download/2026-02-08/marksman-linux-x64
 mv marksman-linux-x64 marksman
 chmod +x marksman
 mv marksman "$HOME/.local/bin/"
